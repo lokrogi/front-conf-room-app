45c3c787f2b5dd2bd74e69abb5f5e6aa0ff0b4d1
igor <kolodziejigor@gmail.com>
2022-05-16 12:41:27 +0200

    Added form for adding conference room

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfRoomsComponent } from './components/conf-rooms/conf-rooms.component';
+import { ConfRoomService } from './services/conf-room.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'conference-room-reservation';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    
+  }
+  
+  public init(): void {
+
+  }
+
+  public openAddForm(): void {
+    let confRoomComponent = new ConfRoomsComponent(null);
+
+    confRoomComponent.openAddForm();
+  }
 }
