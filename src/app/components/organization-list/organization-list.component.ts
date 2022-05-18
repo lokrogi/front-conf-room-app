e38404902bec0e69a9ae2d62c87391d24fcf4998
igor <kolodziejigor@gmail.com>
2022-05-18 15:17:55 +0200

    Organization-list component prepared for backend

--- src/app/components/organization-list/organization-list.component.ts
+++ src/app/components/organization-list/organization-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Organization } from 'src/app/models/organization';
 import { SharedService } from 'src/app/shared/shared.service';
@@ -13,6 +14,10 @@ export class OrganizationListComponent implements OnInit {
   organizations : Organization[] = [];
 
   chosenOrganization : Organization | undefined;
+
+  organizationToEdit : Organization | null = null;
+
+  organizationToDelete : Organization | null = null;
 
   constructor(private router : Router, private sharedService : SharedService) { }
 
@@ -35,4 +40,82 @@ export class OrganizationListComponent implements OnInit {
     console.log(this.chosenOrganization)
 
   }
+
+  public openEditOrganizationModal(organization : Organization) {
+    this.organizationToEdit = organization;
+
+    let editModal = document.getElementById('edit-organization');
+
+    if (editModal != null) {
+      editModal.style.display = 'block';
+    }
+  }
+
+  public closeEditOrganizationModal() {
+    this.organizationToEdit = null;
+    let editModal = document.getElementById('edit-organization');
+
+    if (editModal != null) {
+      editModal.style.display = 'none';
+    }
+  }
+
+  public editOrganization(form : NgForm) {
+    let result = {id : this.organizationToEdit?.id, name : form.value.name} 
+
+    //TODO
+    console.log(result);
+    this.closeEditOrganizationModal();
+  }
+
+  public openDeleteOrganizationModal(organization : Organization) {
+    this.organizationToDelete = organization;
+
+    let deleteModal = document.getElementById('delete-organization');
+
+    if(deleteModal != null) {
+      deleteModal.style.display = 'block';
+    }
+  }
+
+  public closeDeleteOrganizationModal() {
+    this.organizationToDelete = null;
+    let deleteModal = document.getElementById('delete-organization');
+
+    if(deleteModal != null) {
+      deleteModal.style.display = 'none';
+    }
+  }
+
+  public deleteOrganization() {
+    //TODO
+    
+    console.log("Delete org with id " + this.organizationToDelete?.id);
+    this.closeDeleteOrganizationModal();
+  }
+
+  public openAddOrganizationModal() {
+    let addModal = document.getElementById('add-organization');
+
+    if(addModal != null) {
+      addModal.style.display = 'block'
+    }
+  }
+
+  public closeAddOrganizationModal() {
+    let addModal = document.getElementById('add-organization');
+
+    if(addModal != null) {
+      addModal.style.display = 'none'
+    }
+  }
+
+  public addOrganization(form : NgForm) {
+    //TODO
+    
+    console.log(form.value)
+    this.closeAddOrganizationModal();
+  }
+
+
 }
